0d5388d40a01d928e8c1f5d52b280bd5bb35cecb
eigen-spaced <saneyar.k@gmail.com>
2025-08-14 11:21:55 +0400

    Fix deployment errors

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,6 @@ import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
 import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
-import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
 import { SITE } from "@/config";
 
 export const SOCIALS = [
@@ -48,18 +46,6 @@ export const SHARE_LINKS = [
     linkTitle: `Share this post on X`,
     icon: IconBrandX,
   },
-  // {
-  //   name: "Telegram",
-  //   href: "https://t.me/share/url?url=",
-  //   linkTitle: `Share this post via Telegram`,
-  //   icon: IconTelegram,
-  // },
-  // {
-  //   name: "Pinterest",
-  //   href: "https://pinterest.com/pin/create/button/?url=",
-  //   linkTitle: `Share this post on Pinterest`,
-  //   icon: IconPinterest,
-  // },
   // {
   //   name: "Mail",
   //   href: "mailto:?subject=See%20this%20post&body=",
