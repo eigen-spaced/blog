a87c005063d6d06992fe047dd667fdf4a2181c44
eigen-spaced <saneyar.k@gmail.com>
2025-09-02 13:35:37 +0400

    Theme modifications

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 export const SITE = {
   website: "https://stillandbecoming.xyz/", // replace this with your deployed domain
   author: "Sunny Khazin",
-  profile: "https://satnaing.dev/",
+  profile: "",
   desc: "I can't make up my mind on what I want to write about, so I write about everything ",
   title: "Still and Becoming",
   ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
+  lightAndDarkMode: false,
   postPerIndex: 4,
   postPerPage: 8,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
@@ -18,5 +18,5 @@ export const SITE = {
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Australia/Melbourne", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
