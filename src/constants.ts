685d1a3a20360cc9d10439e02c8cd409467f1e67
eigen-spaced <saneyar.k@gmail.com>
2026-04-25 16:18:40 +0400

    Remove unnecessary components

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,6 @@ import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
-import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import { SITE } from "@/config";
 
 export const SOCIALS = [
@@ -28,18 +26,6 @@ export const SOCIALS = [
 ] as const;
 
 export const SHARE_LINKS = [
-  {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
-    linkTitle: `Share this post via WhatsApp`,
-    icon: IconWhatsapp,
-  },
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Share this post on Facebook`,
-    icon: IconFacebook,
-  },
   {
     name: "X",
     href: "https://x.com/intent/post?url=",
